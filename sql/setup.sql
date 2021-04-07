a04df8318b060a90c80c36d2850387fc48c3b75d
SamuelHGerber <henrylightfoot@gmail.com>
2021-04-06 17:40:06 -0700

    local database set up and verified in pg admin

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,17 +3,28 @@ DROP TABLE IF EXISTS accessories;
 
 CREATE TABLE individuals (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    alias TEXT NOT NULL
+    alias TEXT NOT NULL,
+    human BOOLEAN NOT NULL
 );
 
 CREATE TABLE accessories (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    clothing TEXT NOT NULL,
-    handheld TEXT NOT NULL,
-    face TEXT NOT NULL,
-    codeword TEXT NOT NULL,
-    human BOOLEAN NOT NULL,
+    thing TEXT NOT NULL,
+    color TEXT NOT NULL,
     person_id BIGINT NOT NULL REFERENCES individuals(id)
-)
+);
 
-INSERT INTO individuals (alias) VALUES ('Clyde the Transparent Aluminum Salesman'), ('Nina Lately of Misty Hills Farm');
+INSERT INTO individuals (alias, human) VALUES ('Clyde the Transparent Aluminum Salesman', false), ('Nina Lately of Misty Hills Farm', true);
+
+
+
+-- CREATE TABLE accessories (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     thing TEXT NOT NULL,
+--     clothing TEXT NOT NULL,
+--     handheld TEXT NOT NULL,
+--     face TEXT NOT NULL,
+--     codeword TEXT NOT NULL,
+--     human BOOLEAN NOT NULL,
+--     person_id BIGINT NOT NULL REFERENCES individuals(id)
+-- );
