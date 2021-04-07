a45e5ad3f9d2a45152bea83012a1e6c3453140bc
SamuelHGerber <henrylightfoot@gmail.com>
2021-04-06 17:56:11 -0700

    test red for POST /individuals endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,20 @@ describe('lab09-build-something routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('creates and add an individual to the database', () => {
+    return request(app)
+      .post('/api/v1/individuals')
+      .send({
+        alias: 'The Other George Bloom the Lepidopterist',
+        human: true,
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: 3,
+          alias: 'The Other George Bloom the Lepidopterist',
+          human: true,
+        });
+      });
+  });
 });
